cd6920e96a2caf22651a170c5ef3899f25a39a26
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-10 13:39:06 +0100

    Automatic Logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,9 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+    const logoutTimer = setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' });
+    }, 360000);
+
+    return () => {
+      clearTimeout(logoutTimer);
+    };
+  }, [token]);
 
   return (
     <>
@@ -20,4 +36,4 @@ export default RootLayout;
 
 export const loader = () => {
   return localStorage.getItem('ACCESS_TOKEN');
-}
+};
