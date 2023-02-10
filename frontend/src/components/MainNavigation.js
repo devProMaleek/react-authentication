7d21911cad51275ef5413bec000bf1535cf4205d
Abdulmalik Adebayo <milikiadbay@gmail.com>
2023-02-10 12:55:09 +0100

    User Logout function

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Form, NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import NewsletterSignup from './NewsletterSignup';
 
@@ -27,6 +27,13 @@ function MainNavigation() {
               <NavLink to="/auth?mode=login" className={({ isActive }) => (isActive ? classes.active : undefined)}>
                 Authentication
               </NavLink>
+            </li>
+            <li>
+              <Form action='/logout' method='POST'>
+                <button type="submit">
+                  Logout
+                </button>
+              </Form>
             </li>
           </ul>
         </nav>
---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,6 @@
+import { redirect } from 'react-router-dom';
+
+export const action = () => {
+  localStorage.clear();
+  return redirect('/auth?mode=login ');
+};
